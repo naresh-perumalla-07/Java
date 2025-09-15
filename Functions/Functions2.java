36b46731aa35fe5959788ae843675576c14ed983
Naresh Perumalla <naresh.xplores777@gmail.com>
2025-09-15 22:14:42 +0530

    Functions in Java

---
+++ Functions/Functions2.java
@@ -0,0 +1,20 @@
+package Functions;
+
+public class Functions2 {
+    //Static and Non Static
+
+    static void staticMethod() {
+        System.out.println("I am static!");
+    }
+
+    void nonStaticMethod() {
+        System.out.println("I am non-static!");
+    }
+
+    public static void main(String[] args) {
+         Functions2.staticMethod();  // directly call
+
+         Functions2 obj = new Functions2();  // create object
+        obj.nonStaticMethod();
+    }
+}

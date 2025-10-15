c05b74759f9aa2cc3af31b51a008eefa4aad9fab
naresh-perumalla-07 <naresh.xplores777@gmail.com>
2025-10-15 21:18:39 +0530

    Update AdvancedArrays.java

--- Arrays/AdvancedArrays.java
+++ Arrays/AdvancedArrays.java
@@ -2,7 +2,7 @@ package Arrays;
 
 public class AdvancedArrays {
 
-    // 1. Prefix Sumfasdfsadfdfsadfsdafdsfsadfsadffsdafdsfsdasfddasfdffsdafsdafasdfadfadfadfafdasdfsdfasfasfasfddssdafdsaf
+    // 1. Prefix Sumfasdfsadfdfsadfsdafdsfsadfsadffsdsdf
 f
     public static void prefixSumExample() {
         int[] arr = {1, 2, 3, 4, 5};

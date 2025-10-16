d20c4022d386cab34c12a0120a0f754e4b3ccd8b
naresh-perumalla-07 <naresh.xplores777@gmail.com>
2025-10-16 23:29:05 +0530

    Update Functions.java

--- Functions/Functions.java
+++ Functions/Functions.java
@@ -6,7 +6,7 @@ public class Functions {
     //Syntax 
 
     // returnType methodName(parameters) {
-    // // bodyfdgsadfasdf
+    // // bodyfdgsadfasdfasdfasf
     // return value;  // if returnType is not void
     // }
 

70e3d38966564360f0d23b598aa39af58599e796
naresh-perumalla-07 <naresh.xplores777@gmail.com>
2025-10-18 21:08:47 +0530

    Updated DataTypes.java

--- JavaBasics/DataTypes.java
+++ JavaBasics/DataTypes.java
@@ -12,7 +12,7 @@ public class DataTypes {
         System.out.println("Grade: " + grade);
         System.out.println("Is Java Fun? " + isJavaFun);
 
-        // Example: changing valuegfhdfgfdgdfgdfggfdsgdf
+        // Example: changing value
         age = 22;
         System.out.println("Updated Age: " + age);
     }

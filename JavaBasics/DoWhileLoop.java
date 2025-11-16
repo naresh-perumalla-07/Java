a3c4e6a2f3ebef2da36a004b3257a79751f0dd6c
naresh-perumalla-07 <naresh.xplores777@gmail.com>
2025-11-16 22:39:26 +0530

    Updated DoWhileLoop.java

--- JavaBasics/DoWhileLoop.java
+++ JavaBasics/DoWhileLoop.java
@@ -4,7 +4,7 @@ class DoWhileLoop {
     public static void main(String[] args){ 
         int i = 1;
         do{ 
-            System.out.println("i = " + i);
+            System.out.println("i = " + i); 
             i++;
         } while(i <= 5);
     }

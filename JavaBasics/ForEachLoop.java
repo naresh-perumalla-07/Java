c9d622dce4b6d82615154a943892cf620f0f80ce
naresh-perumalla-07 <naresh.xplores777@gmail.com>
2025-11-20 20:18:01 +0530

    Updated ForEachLoop.java

--- JavaBasics/ForEachLoop.java
+++ JavaBasics/ForEachLoop.java
@@ -5,6 +5,6 @@ class ForEachLoop {
         int numbers[] = {10, 20, 30, 40};
 
         for(int num : numbers){ 
-            System.out.println(num); //10 20   
+            System.out.println(num); //10 20 30 40
     }
 }

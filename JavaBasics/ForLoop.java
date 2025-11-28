e96d04121c8ea700d69c1508e7d6380d336cc7a5
naresh-perumalla-07 <naresh.xplores777@gmail.com>
2025-11-28 21:22:49 +0530

    Updated ForLoop.java

--- JavaBasics/ForLoop.java
+++ JavaBasics/ForLoop.java
@@ -5,8 +5,6 @@ public class ForLoop{
 
         //Syntax for For Loop
 
-        // for(initialization; condition; increment/decrement){ // code }
-
         for(int i=1; i<=5; i++){
             System.out.println("i = " + i);
         }
